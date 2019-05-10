694c28db5b965a4ad91ba9d66c140dbcffbe61d4
Danniel Isiah J. Libor <disaiahlibor@gmail.com>
2019-05-10 21:21:02 +0800

    title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <title>Laravel</title>
+        <title>Weather Checker</title>
 
         <!--begin::Web font -->
 		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
